c6c591961a93220778a14d5bd9e753c5a6997020
nishantaggarwal <naggarwal@mafcarrefour.com>
2025-04-09 12:38:44 +0530

    update

---
+++ src/main/java/com/luv2code/aopApp/aspects/LoggingAspect.java
@@ -0,0 +1,55 @@
+package com.luv2code.aopApp.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+//    @Before("execution(* orderPackage(..))")
+//    @Before("execution(* com.luv2code.aopApp.services.impl.ShipmentServiceImpl.orderPackage(..))")
+//    @Before("execution(* com.luv2code.aopApp.services.impl.*.orderPackage(..))")
+    @Before("execution(* com.luv2code.aopApp.services.impl.*.*(..))")
+    public void beforeOrderPackage(JoinPoint joinPoint){
+        log.info("Before called from Logging Aspect kind, {}",joinPoint.getKind());
+        log.info("Before called from Logging Aspect signature, {}",joinPoint.getSignature());
+    }
+
+//    @Before("within(com.luv2code.aopApp.services.impl.*)")
+    @Before("within(com.luv2code.aopApp..*)")
+    public void beforeServiceImplCalls(){
+        log.info("Service impl calls");
+    }
+
+    @Before("@annotation(org.springframework.transaction.annotation.Transactional)")
+    public void beforeTransactionalAnnotationCalls(){
+        log.info("Before Transactional Annotations Calls");
+    }
+
+    @Before("@annotation(com.luv2code.aopApp.aspects.MyLogging)")
+    public void beforeMyLoggingAnnotationCalls(){
+        log.info("Before MyLogging Annotations Calls");
+    }
+
+    @Before("myLoggingAndAopMethodsPointCut()")
+    public void beforeMyLoggingAndAopMethodsPointCut(){
+        log.info("Before MyLogging Annotations Calls And Aop Methods");
+    }
+
+    @After("myLoggingAndAopMethodsPointCut()")
+    public void afterMyLoggingAndAopMethodsPointCut(){
+        log.info("After MyLogging Annotations Calls And Aop Methods");
+    }
+
+    @Pointcut("@annotation(com.luv2code.aopApp.aspects.MyLogging) && within(com.luv2code.aopApp..*)")
+    public void myLoggingAndAopMethodsPointCut(){
+
+    }
+}
---
+++ src/test/java/com/luv2code/aopApp/AopAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.luv2code.aopApp;
+
+import com.luv2code.aopApp.services.ShipmentService;
+import com.luv2code.aopApp.services.impl.ShipmentServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopAppApplicationTests {
+}
